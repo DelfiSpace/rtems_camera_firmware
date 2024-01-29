7d35187b30cb0b73cd628d5de43ba9a0cf16060e
Vincenzo Calabretta <vincenzo.cal.main@gmail.com>
2024-01-29 10:55:01 +0000

    simple frame interrupt implementation rtems framework

--- hello.c
+++ hello.c
@@ -4,8 +4,8 @@
 // TODO: Include the following headers into asystem header that you can include
 // only once.
 //
-#include <hwlist_handlers.h>
 #include <rtems.h>
+#include <rtems/irq.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,6 +14,7 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <hwlist_handlers.h>
 #include <stm32l4r9_module_clk_config.h>
 #include <stm32l4r9_module_dcmi.h>
 #include <stm32l4r9_module_i2c.h>
@@ -40,7 +41,11 @@
 
 struct Node *hw_head = NULL;
 
-void DCMI_IRQHandler(void); // XXX:
+// Prototype for the ISR
+rtems_isr DCMI_frame_isr_handler(rtems_vector_number vector); // XXX:
+
+// Prototype
+rtems_status_code register_dcmi_frame_isr(void);
 
 rtems_task Init(rtems_task_argument ignored) {
   // ------------ SYTSTEM INITIALIZATION  -------------------------------------
@@ -51,19 +56,13 @@ rtems_task Init(rtems_task_argument ignored) {
   hwlist_require(&hw_head, &mspi_init, NULL);
   uart_write_buf(USART2, "play victory dance\n\r", 22); // XXX:
 
-  //* configures interrupts */
-  // sets handler pointer in vector table
-  __NVIC_SetVector(DCMI_IRQn, (u32)DCMI_IRQHandler);
-  // sets priority
-  NVIC_SetPriority(DCMI_IRQn, 0x5C);
-  // enable the DCMI interrupt
-  NVIC_EnableIRQ(DCMI_IRQn);
-
-  // enable dcmi vsync interrupt
-  DCMI->IER |= DCMI_IER_FRAME_IE;
-
+  rtems_status_code ir_rs = {0};
+  ir_rs = register_dcmi_frame_isr();
   // enable interrupts
-  __enable_irq();
+  //__enable_irq();
+
+  /* enable dcmi vsync interrupt */
+  DCMI->IER |= DCMI_IER_FRAME_IE;
 
   /* set dcmi capture flag */
   DCMI->CR |= DCMI_CR_CAPTURE;
@@ -73,17 +72,28 @@ rtems_task Init(rtems_task_argument ignored) {
   }
   exit(0);
 }
-void DCMI_IRQHandler(void) {
-  uart_write_buf(USART2, "received frame  \n\r", 20); // XXX:
 
-  struct jpeg_image test_image;
-  u32 *image_buffer = dcmi_get_buffer_ptr();
-  dcmi_buffer_analisis(&test_image, image_buffer);
+// Register the interrupt
+rtems_status_code register_dcmi_frame_isr(void) {
+  rtems_status_code status;
 
-  /* clear at the end, to avoid overruns? or non issue due to priority?
-   * what happens if they have the same priority?
-   */
-  /* you need a multiple interrupt handler in case more sources are enabled */
+  // Define the interrupt vector
+  rtems_vector_number dcmi_interrupt_vector_n = DCMI_IRQn;
+
+  // Register the ISR
+  status =
+      rtems_interrupt_handler_install(dcmi_interrupt_vector_n, // Vector number
+                                      "DCMI_frame", // Name for the ISR
+                                      RTEMS_INTERRUPT_UNIQUE, // Flags
+                                      DCMI_frame_isr_handler, // ISR
+                                      NULL                    // ISR argument
+      );
+  return status;
+}
+
+// Implementation of the ISR
+rtems_isr DCMI_frame_isr_handler(rtems_vector_number vector) {
+  uart_write_buf(USART2, "rtems interrupt received frame  \n\r", 34); // XXX:
   DCMI->ICR &= ~(DCMI_ICR_FRAME_ISC_Msk);
 }
 
